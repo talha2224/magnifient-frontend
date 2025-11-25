9bbb411c665d993e041b7481c32fc5794a665109
Talha Haider <talhahaider074@gmail.com>
2025-11-25 21:26:37 +0500

    Update Vite config for server and preview settings
    Added server and preview configuration for allowed hosts.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // important for container hosting
+    allowedHosts: ["*.ondigitalocean.app"],
+  },
+  preview: {
+    host: true,
+    allowedHosts: ["*.ondigitalocean.app"],
+  },
+});
